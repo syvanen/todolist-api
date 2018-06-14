3fc2eaf8a148c5918e1a0478d66e4cf117ed22e5
donal <donalspring@gmail.com>
2018-06-14 22:25:37 +0100

    FIX - issue with NODE_ENV

--- server/config/express.js
+++ server/config/express.js
@@ -26,16 +26,9 @@ module.exports = function(app) {
   app.use(methodOverride());
   app.use(cookieParser());
   if ('production' === env  || 'dev' === env  || 'test' === env) {
-    // app.use(express.static(path.join(config.root, 'public')));
-    // app.set('appPath', path.join(config.root, 'public'));
     app.use(morgan('dev'));
-  }
-
-  if ('development' === env || 'test' === env) {
-    app.use(require('connect-livereload')());
-    app.use(express.static(path.join(config.root, '.tmp')));
-    app.use(express.static(path.join(config.root, 'client')));
-    app.set('appPath', path.join(config.root, 'client'));
+  } else {
+    // else 'development'
     app.use(morgan('dev'));
     app.use(errorHandler()); // Error handler - has to be last
   }
