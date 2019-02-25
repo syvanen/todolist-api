c292b8d9ac725b0e705c32982d49823c4b84c1d1
Toni Syvanen <syvanen@users.noreply.github.com>
2019-02-25 11:48:31 +0200

    Update development.js

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/todolist-dev'
+    uri: 'mongodb://username:password@' + process.env.MONGODB_SERVICE_HOST + ':' + process.env.MONGODB_SERVICE_PORT + '/todolist',
   },
   mocks: {
     // enable this for non mock api
